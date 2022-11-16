f76d8b6defa92facf5c8a4cdd75b05b6f122fa8d
MattiaColombari <mattia.colombari00@gmail.com>
2022-11-16 16:44:46 +0100

    Generic popup structure defined.

--- app/src/main/java/com/example/justbirthday/AddFriendActivity.java
+++ app/src/main/java/com/example/justbirthday/AddFriendActivity.java
@@ -2,13 +2,49 @@ package com.example.justbirthday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
-public class AddFriendActivity extends AppCompatActivity {
+public class AddFriendActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //This line will enable full screen.
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_add_friend);
+
+        test = (TextView) findViewById(R.id.textView5);
+
+        test.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.generic_popup, null);
+
+        PopupWindow popupWindow = new PopupWindow(
+                popupView,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.setElevation(20);
+        }
+        popupWindow.setAnimationStyle(R.style.AnimationGenericPopupWindow);
+        popupWindow.update();
+        /* "v" is used as a parent view to get the View.getWindowToken() token from. */
+        popupWindow.showAtLocation(v, Gravity.TOP, 0, 300);
     }
 }
